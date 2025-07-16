ff26b1944b5b9ac3dbaf103a07737929840e0617
= <=>
2025-07-16 21:46:19 +0300

    add lesson1js

---
+++ JavaScript/Lesson1/js/math.js
@@ -0,0 +1,27 @@
+// task 1
+// let num = prompt('enter number');
+// let result = num ** 2;
+// console.log(`Result is ${result}`);
+
+// task 2
+// let num1 = +prompt('enter number 1');
+// let num2 = Number(prompt('enter number 2'));
+// console.log(`Result is ${(num1 + num2) / 2}`);
+
+// task 3
+// const CONVERTER_IN_MILES = 0.621371;
+// let promptKm = +prompt('Enter km');
+// console.log(`Result - ${promptKm * CONVERTER_IN_MILES} miles`);
+
+// task 5
+// let number1 = +prompt('enter number 1');
+// let number2 = +prompt('enter number 2');
+// console.log(`${number1} + ${number2} = ${number1 + number2}`);
+// console.log(`${number1} - ${number2} = ${number1 - number2}`);
+// console.log(`${number1} * ${number2} = ${number1 * number2}`);
+// console.log(`${number1} / ${number2} = ${number1 / number2}`);
+
+// task 8
+let number = prompt('enter number');
+
+console.log();
---
+++ JavaScript/Lesson1/js/script.js
@@ -0,0 +1,27 @@
+// let admin;
+// let name;
+
+// name = 'John';
+// admin = name;
+
+// console.log(admin);
+
+
+let name = 'John';
+let age = 50;
+let city;
+let bigger = 1 > 3
+
+let info = `${name} is ${age} years old`;
+
+console.log(info);
+console.log(bigger);
+
+console.log(typeof(age));
+console.log(typeof(name));
+console.log(typeof(city));
+console.log(typeof(bigger));
+
+// let hello = prompt('hello');
+// console.log(`Hello, ${hello}`);
+
