6155b0288c283d6570cc5835bc49a76925a74145
= <=>
2025-09-03 21:46:59 +0300

    add lesson8js

---
+++ JavaScript/Lesson8/script.js
@@ -0,0 +1,52 @@
+// 1.1
+
+const car1 = {
+  manufacturer: 'Mercedes',
+  model: 's350',
+  year: 1977,
+  speed: 78,
+}
+
+function carOnScreen(obj) {
+  console.log(`The car ${obj.manufacturer} released, model ${obj.model} in ${obj.year}, has a speed of ${obj.speed}`);
+}
+
+// function carsTime(dis, car) {  
+//   let time = dis / car.speed;
+//   let relax = 0;
+// }
+
+carOnScreen(car1);
+
+// carsTime(2000, car1);
+//--------------------------------------------------------
+
+
+// 1.2
+
+// numerator
+// denomerator
+
+// class Fraction {
+//   constructor(num, denom) {
+//     this.num = num;
+//     this.denom = denom;
+//   }
+// }
+
+// const fr = new Fraction(1, 4);
+// console.log(fr);
+
+
+
+
+let hello = 'hellooo!!';
+let counter = 0;
+
+// while(counter < 10) {
+//   console.log(`${counter + 1} ${hello}`);
+//   counter++;
+// }
+
+
+
