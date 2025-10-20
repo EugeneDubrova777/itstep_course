a7e3e7c48ca9b42957811381c4151a1f0ea57fec
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2025-10-20 21:47:29 +0300

    add regex to artefuckts

--- Artefuckts/js/script.js
+++ Artefuckts/js/script.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
       currentPageIndex--;
     }
     setSliderPagination();
+    $('#form-login').validate();
   });
   
   $('#slider-right').click(function() {
@@ -42,6 +43,27 @@ $(document).ready(function(){
 });
 
 function validate() {
-  return false;
+  let isValid = true;
+  let form = document.forms[0];
+  let name = form.elements[0];
+  let email = form.elements[1];
+
+  let name_regex = /^\s*[A-Za-zА-Яа-я]{2,20}\s*[A-Za-zА-Яа-я]{0,20}\s*$/;
+  let email_regex = /^[a-z_\-0-9\.\*]+@[a-z_\-0-9]+\.[a-z]{2,3}$/;
+
+  name.classList.remove('is-invalid');
+  if (!name_regex.test(name.value)) {
+    name.classList.add('is-invalid');
+    isValid = false;
+  }
+
+  email.classList.remove('is-invalid');
+  if (!email_regex.test(email.value)) {
+    email.classList.add('is-invalid');
+    isValid = false;
+  }
+  
+  return isValid;
 }
+
 
