e86e855d9dccff61ab311a9d95e88fc954457722
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2026-02-18 21:43:02 +0300

    add different shit

---
+++ JavaScript/Lesson38/script.js
@@ -0,0 +1,11 @@
+// let user = {
+//   adress: 'ddd'
+// }
+
+// console.log(user?.adress?.street);
+
+
+
+let time = new Date('2026-02-17');
+time = Math.floor(Date.now() / 1000 / 60 / 60 / 24 / 365);
+console.dir(time);
